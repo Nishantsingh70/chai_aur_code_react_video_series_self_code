d9baa292fbb0fa9d9642665f4c2f40a6fc7a745b
Nishant Singh <64470189+Nishantsingh70@users.noreply.github.com>
2024-01-13 16:43:33 +0530

    Update server.js

--- React_Full_Stack_Basic/Backend/server.js
+++ React_Full_Stack_Basic/Backend/server.js
@@ -1,13 +1,20 @@
-// There are two ways to import the package one is common js and second is module js
-// for cors you can use proxy in front app or you can whitelist the urls from server (backend)
+/*
+  npm init -y
+  npm install express
+*/
 
-// const express = require('express') => code will come in Synchronously manner.
-import express from 'express'  // code will come asynchronously manner  // module js
+// There are two ways to import the package one is common js and second is module js
+// for cors you can use proxy in frontend app or you can whitelist the urls from server (backend)
+// cors :- either your URL is different or your port is different then it will consider as cors. 
+// we have cors npm package also in market for more information.
+
+// const express = require('express') => code will come in Synchronously manner.  //common js syntax
+import express from 'express'  // code will come asynchronously manner  //module js syntax
 
 const app = express();
 
 // first build the react app then move that dist folder into backend and add one line in the backend code.
-// app.use(express.static('dist'))  // This is middleware we can recognize middleware by use method.
+// app.use(express.static('dist'))  // This is middleware we can recognize middleware by "use" method.
 
 
 // app.use(express.static('dist')) 
@@ -21,27 +28,27 @@ app.get('/api/jokes', (req, res) => {
     const jokes = [
         {
             id: 1,
-            litle: 'A joke',
+            title: 'A joke',
             content: 'This is a joke'
         },
         {
             id: 2,
-            litle: 'Another joke',
+            title: 'Another joke',
             content: 'This is another joke'
         },
         {
             id: 3,
-            litle: 'A third joke',
+            title: 'A third joke',
             content: 'This is a third joke'
         },
         {
             id: 4,
-            litle: 'A fourth joke',
+            title: 'A fourth joke',
             content: 'This is a fourth joke'
         },
         {
             id: 5,
-            litle: 'A fifth joke',
+            title: 'A fifth joke',
             content: 'This is a fifth joke'
         }
     ]
@@ -53,4 +60,6 @@ const port = process.env.port || 3000
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
+
+
 
