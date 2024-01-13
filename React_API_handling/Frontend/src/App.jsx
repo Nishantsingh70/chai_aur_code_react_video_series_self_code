d58c0c45aa77ac1afcc5b9e0c9a65a60b4602d97
Nishant Singh <64470189+Nishantsingh70@users.noreply.github.com>
2024-01-13 16:47:05 +0530

    Update App.jsx

--- React_API_handling/Frontend/src/App.jsx
+++ React_API_handling/Frontend/src/App.jsx
@@ -1,75 +1,29 @@
+/* 
+  npm create vite@latest .  (here . is used for creating all the file/folder in same folder where we are.)
+  npm install 
+  npm install axios
+*/
+
+/* 
+  If we add  
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  in vite.config.js file then before /api request this localhost url will add and server will assume that
+  this request is coming from same URL and port number as backend.
+*/
+
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
 
-function App() {
-
-  const [products, setProducts] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [search,setSearch] = useState('')
-  const controller = new AbortController()
-  // we avoid the race condition by using AbortController methods.
-
-  useEffect(()=> {
-  
-     // we used IIFE because for await we have to make the function async so we used IIFE here.
-    ;(async() => {
-    try {
-        setLoading(true)
-        setError(false)
-        const response = await axios.get('/api/products?search=' + search, {
-         // this controller will cancel all the old request on a particular URL, but send those in catch block. 
-          signal: controller.signal   
-        })
-        console.log(response.data)
-        setProducts(response.data)
-        setLoading(false)
-    } catch (error) {
-        if(axios.isCancel(error)){
-          console.log('Request canceled: ', error.message)
-          return
-        }
-        setError(true)   
-        setLoading(false)
-    }
-    })()
-
-    // clearnup code
-    return () => {
-      controller.abort()
-    }
-  },[search])
-
-
-  if (error){
-    <h1>Something went wrong.</h1>
-  }
-
-  if (loading){
-    <h1>Loading..</h1>
-  }
-
-  return (
-    <>
-      <h1>Chai and API in react.</h1>
-      <input type="text" placeholder="Search"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}/>
-      {loading && (<h1>Loading..</h1>)}
-      {error && (<h1>Something went wrong.</h1>)}
-      <h2>Number of Products are: {products.length}</h2>
-    </>
-  )
-}
-
-export default App
-
-
 /*
+// Approach1:
 
-function App() {
+export default function App() {
   const [products, setProducts] = useState([])
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -93,18 +47,60 @@ function App() {
   },[])
 
   if (error){
-    <h1>Something went wrong.</h1>
+    return <h1>Something went wrong.</h1>
   }
 
   if (loading){
-    <h1>Loading..</h1>
+    return <h1>Loading..</h1>
   }
 
   return (
     <>
       <h1>Chai and API in react.</h1>
-      {loading && (<h1>Loading..</h1>)}
-      {error && (<h1>Something went wrong.</h1>)}
+      <h2>Number of Products are: {products.length}</h2>   
+    </>
+  )
+}
+
+*/
+
+/*
+// Approach2:
+
+export default function App() {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=> {
+    // we used IIFE because for await we have to make the function async so we used IIFE here.
+    (async() => {
+    try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get('/api/products')
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+    } catch (error) {
+        console.log(error) 
+        setError(true)   
+        setLoading(false)
+    }
+    })()
+  },[])
+
+  if (error){
+    return <h1>Something went wrong.</h1>
+  }
+
+  if (loading){
+    return <h1>Loading..</h1>
+  }
+
+  return (
+    <>  
+      <h1>Chai and API in react.</h1>
       <h2>Number of Products are: {products.length}</h2>
     </>
   )
@@ -113,19 +109,20 @@ function App() {
 */
 
 
-
 /*
 
-function App() {
+// Approach3:
+
+export default function App() {
 
   const [products, error, loading] = customReactQuery('/api/products')
 
   if (error){
-    <h1>Something went wrong.</h1>
+    return <h1>Something went wrong.</h1>
   }
 
   if (loading){
-    <h1>Loading..</h1>
+    return <h1>Loading..</h1>
   }
 
   return (
@@ -163,3 +160,107 @@ const customReactQuery = (urlPath) => {
 }
 
 */
+
+/*
+
+// Approach4:
+
+export default function App() {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=> {
+    // we used IIFE because for await we have to make the function async so we used IIFE here.
+    (async() => {
+    try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get('/api/products')
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+    } catch (error) {
+        console.log(error) 
+        setError(true)   
+        setLoading(false)
+    }
+    })()
+  },[])
+
+  return (
+    <>  
+      <h1>Chai and API in react.</h1>
+      {loading && (<h1>Loading..</h1>)}
+      {error && (<h1>Something went wrong.</h1>)}
+      <h2>Number of Products are: {products.length}</h2>
+    </>
+  )
+}
+
+*/
+
+//Approach5: 
+
+export default function App() {
+
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
+  const controller = new AbortController()
+  // we avoid the race condition by using AbortController methods.
+
+  useEffect(()=> {
+  
+     // we used IIFE because for await we have to make the function async so we used IIFE here.
+    ;(async() => {
+    try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get('/api/products?search=' + search, {
+         // this controller will cancel all the old request on a particular URL, but send those in catch block. 
+          signal: controller.signal   
+        })
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+    } catch (error) {
+        if(axios.isCancel(error)){
+          console.log('Request canceled: ', error.message)
+          return
+        }
+        setError(true)   
+        setLoading(false)
+    }
+    })()
+
+    // clearnup code
+    return () => {
+      controller.abort()
+    }
+  },[search])
+
+
+  if (error){
+    return <h1>Something went wrong.</h1>
+  }
+
+  if (loading){
+    return <h1>Loading..</h1>
+  }
+
+  return (
+    <>
+      <h1>Chai and API in react.</h1>
+      <input type="text" placeholder="Search"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}/>
+      {loading && (<h1>Loading..</h1>)}
+      {error && (<h1>Something went wrong.</h1>)}
+      <h2>Number of Products are: {products.length}</h2>
+    </>
+  )
+}
+
+
